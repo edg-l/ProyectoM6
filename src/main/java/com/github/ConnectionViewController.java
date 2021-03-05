d4714860d9a6446fc9b631801a1c7aeecbccca17
Kevin <kvn2018@gmail.com>
2021-03-05 12:30:27 +0100

    views/controllers

---
+++ src/main/java/com/github/ConnectionViewController.java
@@ -0,0 +1,29 @@
+package com.github;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ConnectionViewController {
+
+	@FXML
+	private Button btnLogin;
+	
+	private void clickBtnLogin() {
+		
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("ClientListView" + ".fxml"));
+		Scene scene;
+		try {
+			scene = new Scene(fxmlLoader.load(), 600, 300);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException io) {
+			io.printStackTrace();
+		}
+	}
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.github {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
     opens com.github to javafx.fxml;
     exports com.github;
